cb650806b2aed3de8dbb66ba9c144704bfd6f4a0
Arthur Cesarino <cesarinoarthur@gmail.com>
2023-05-10 21:01:44 -0300

    feat:start emit event on socket.io

---
+++ Server/file.js
@@ -0,0 +1,7 @@
+const a = {
+  nome: "arthur",
+  idade: 24,
+};
+const b = { ...a, nome: "xuxu" };
+
+console.log(a, b);
--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,38 @@
-// const express = require("express")
-// const http = require("http")
 const io = require("socket.io")(3000, {
-    cors: {
-      origin: ['http://localhost:8080'],
-    },
-  })
+  cors: {
+    origin: ["http://localhost:8080"],
+  },
+});
+const users = [];
+io.on("connection", (socket) => {
+  console.log("socket is ready for connection");
+  socket.on("joinRoom", ({ ...roomObject }) => {
+    const user = {
+      id: socket.id,
+      username: roomObject.user.name,
+      room: roomObject.room_uuid,
+      user_id: roomObject.user.user_uuid,
+    };
+    users.push(user);
+    socket.join(user.room);
+    socket.emit("message", "Welcome to application" + user.username);
+    socket.broadcast
+      .to(user.room)
+      .emit("message", `${user.username} has joined the call`);
 
-// const app = express()
-// const server = http.createServer(app)
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: users.filter((queueUser) => queueUser.room === user.room),
+    });
+    io.to(user.room).emit("roomSettings", {
+      ...roomObject,
+    });
+  });
 
-
-io.on("connection", socket => {
-    console.log(socket.id);
-})
+  socket.on("teste", () => {
+    console.log("teste enviado");
+    //socket.join("teste");
+    socket.emit("teste", "Teste enviado");
+    socket.broadcast.emit("teste", "Teste enviado");
+  });
+});
