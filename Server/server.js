fe4b172205dc916d67d4ac5c9b614bfbe5aaa644
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-05-11 01:29:43 -0300

    feature: add events for join queue, disconnect, return position

--- Server/server.js
+++ Server/server.js
@@ -2,37 +2,117 @@ const io = require("socket.io")(3000, {
   cors: {
     origin: ["http://localhost:8080"],
   },
-});
-const users = [];
+})
+
+const users = []
+
 io.on("connection", (socket) => {
-  console.log("socket is ready for connection");
-  socket.on("joinRoom", ({ ...roomObject }) => {
+  console.log(`${socket.id} has connnected`)
+
+  socket.on("disconnect", () => {
+    const index = users.findIndex((user) => {
+        if (user.id === socket.id) {
+          return true
+        }
+      })
+
+    if (index > -1) {
+      users.splice(index, 1)
+      console.log("Um usuário se desconectou:", socket.id)
+      socket.emit("message", "Você foi desconectado")
+    }
+  })
+
+  socket.on("joinQueue", ({ ...queueObject }) => {
     const user = {
       id: socket.id,
-      username: roomObject.user.name,
-      room: roomObject.room_uuid,
-      user_id: roomObject.user.user_uuid,
-    };
-    users.push(user);
-    socket.join(user.room);
-    socket.emit("message", "Welcome to application" + user.username);
-    socket.broadcast
-      .to(user.room)
-      .emit("message", `${user.username} has joined the call`);
+      username: queueObject.user.name,
+      queue: queueObject.room_id,
+      user_id: queueObject.user.user_id, //PRECISA MSM DISSO?
+    }
 
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: users.filter((queueUser) => queueUser.room === user.room),
-    });
-    io.to(user.room).emit("roomSettings", {
-      ...roomObject,
-    });
-  });
+    users.push(user)
+    socket.join(user.queue)
+    socket.emit("message", "Welcome to application " + user.username)
+    console.log(`${user.username} is ready for connection`)
+
+    socket.broadcast
+      .to(user.queue)
+      .emit("message", `${user.username} has joined the call`)
+
+    io.to(user.queue).emit("queueUsers", {
+      queue: user.queue,
+      users: users.filter((queueUser) => queueUser.queue === user.queue),
+    })
+
+    io.to(user.queue).emit("queueSettings", {
+      ...queueObject,
+    })
+  })
+
+  socket.on("queuePosition", () => {
+    for (let position = 0; position < users.length; position++) {
+      const user = users[position]
+      if (user.id === socket.id) {
+        socket.emit("position", {
+          position: position + 1,
+        })
+        return
+      }
+    }
+  })
+
+  socket.on("answerNext", () => {
+    if ( undefined === users.shift()) { //CHECAR SE A FILA ESTA VAZIA
+        console.log("Fila vazia");
+    } else {
+        // RETIRAR O PRIMEIRO DA FILA
+    }
+
+  })
 
   socket.on("teste", () => {
-    console.log("teste enviado");
+    console.log("teste enviado")
     //socket.join("teste");
-    socket.emit("teste", "Teste enviado");
-    socket.broadcast.emit("teste", "Teste enviado");
-  });
-});
+    socket.emit("teste", "Teste enviado")
+    socket.broadcast.emit("teste", "Teste enviado")
+  })
+})
+
+// io.on("connection", (socket) => {
+//     console.log("Um usuário se conectou:", socket.id)
+
+//     waitingQueue.push(socket.id)
+//     totalClients++
+
+//     socket.on("disconnect", () => {
+//       console.log("Um usuário se desconectou:", socket.id)
+//       const index = waitingQueue.indexOf(socket.id)
+//       if (index !== -1) {
+//         waitingQueue.splice(index, 1)
+//         io.emit("userDisconnected", { userId: socket.id })
+//       }
+//     })
+
+//     io.to(socket.id).emit("queuePosition", {
+//       position: waitingQueue.length,
+//       averageWaitingTime:
+//         totalClients === 1 ? 0 : averageWaitingTime / (totalClients - 1),
+//     })
+
+//     socket.on("sessionStarted", (data) => {
+//       const { waitingTime } = data
+//       totalWaitingTime += waitingTime
+//       averageWaitingTime = totalWaitingTime / totalClients
+
+//       const index = waitingQueue.indexOf(socket.id)
+//       if (index !== -1) {
+//         waitingQueue.splice(index, 1)
+//       }
+
+//       io.emit("queueUpdate", {
+//         waitingQueue,
+//         averageWaitingTime,
+//       })
+//     })
+//   })
